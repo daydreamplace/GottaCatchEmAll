961ba8d1d184568969d1bcf6f2647ddbbb937cb5
Eden <you8inpark@gmail.com>
2025-01-02 00:21:40 +0900

    feat: MainViewModel 셋업

--- GottaCatch'EmAll/ViewModel/MainViewModel.swift
+++ GottaCatch'EmAll/ViewModel/MainViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import RxSwift
+
+class MainViewModel {
+    private let networkManager = NetworkManager.shared
+    private let disposeBag = DisposeBag()
+    
+    var pokemonList: BehaviorSubject<[PokemonListItem]> = BehaviorSubject(value: [])
+    
+    var error: PublishSubject<String> = PublishSubject()
+
+    private var limit: Int = 20
+    private var offset: Int = 0
+}
