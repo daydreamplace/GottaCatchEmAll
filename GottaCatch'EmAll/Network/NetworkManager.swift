3306f7b05646488638d1b15a7a34beb19df6a881
Eden <you8inpark@gmail.com>
2025-01-01 12:25:29 +0900

    feat: 제네릭 fetch 메서드와 Moya를 사용해 NetworkManager 구현

--- GottaCatch'EmAll/Network/NetworkManager.swift
+++ GottaCatch'EmAll/Network/NetworkManager.swift
@@ -7,10 +7,36 @@
 
 import Foundation
 import Moya
+import RxSwift
 
 final class NetworkManager {
     static let shared = NetworkManager()
     private init() {}
     
     private let provider = MoyaProvider<PokemonAPI>()
+    
+
+    func fetch<T: Decodable>(endpoint: PokemonAPI, type: T.Type) -> Single<T> {
+        return Single.create { [weak self] single in
+            guard let self = self else {
+                single(.failure(NSError(domain: "NetworkManager", code: -1, userInfo: [NSLocalizedDescriptionKey: "Self is nil."])))
+                return Disposables.create()
+            }
+
+            self.provider.request(endpoint) { result in
+                switch result {
+                case .success(let response):
+                    do {
+                        let decodedData = try JSONDecoder().decode(T.self, from: response.data)
+                        single(.success(decodedData))
+                    } catch {
+                        single(.failure(error))
+                    }
+                case .failure(let error):
+                    single(.failure(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
 }
