85228984745f2f8b165c577262055fe25de577f4
Eden <you8inpark@gmail.com>
2025-01-02 18:03:05 +0900

    feat: pokemon 이미지를 보여줄 PokemonCollectionView 생성

--- GottaCatch'EmAll/View/Main/MainViewController.swift
+++ GottaCatch'EmAll/View/Main/MainViewController.swift
@@ -17,6 +17,20 @@ final class MainViewController: UIViewController {
         return imageView
     }()
     
+    private lazy var pokemonCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        layout.itemSize = CGSize(width: (view.frame.width - 40) / 3, height: (view.frame.width - 40) / 3)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.register(PokemonCollectionViewCell.self, forCellWithReuseIdentifier: "PokemonCell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .mainRed
@@ -24,12 +38,18 @@ final class MainViewController: UIViewController {
     }
     
     func setupViews() {
-        view.addSubviews(pokemonBallImageView)
+        view.addSubviews(pokemonBallImageView, pokemonCollectionView)
         
         pokemonBallImageView.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(10)
             make.centerX.equalToSuperview()
             make.height.width.equalTo(100)
+        }
+        
+        pokemonCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(pokemonBallImageView.snp.top).offset(120)
+            make.left.right.equalToSuperview().inset(10)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
         }
         
     }
---
+++ GottaCatch'EmAll/View/Main/PokemonCell.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonCell.swift
+//  GottaCatch'EmAll
+//
+//  Created by Eden on 1/2/25.
+//
+
+import Foundation
