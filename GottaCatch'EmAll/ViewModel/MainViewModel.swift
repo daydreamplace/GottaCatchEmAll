822a6b177f85a04d3b3efc03dbad1c12c9665286
Eden <you8inpark@gmail.com>
2025-01-02 11:30:44 +0900

    feat: MainViewModel에 fetchPokemonList 로직 구현

--- GottaCatch'EmAll/ViewModel/MainViewModel.swift
+++ GottaCatch'EmAll/ViewModel/MainViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
 
 class MainViewModel {
     private let networkManager = NetworkManager.shared
@@ -15,7 +16,22 @@ class MainViewModel {
     var pokemonList: BehaviorSubject<[PokemonListItem]> = BehaviorSubject(value: [])
     
     var error: PublishSubject<String> = PublishSubject()
-
+    
     private var limit: Int = 20
     private var offset: Int = 0
+    
+    func fetchPokemonList() {
+        let endpoint = PokemonAPI.fetchPokemonList(limit: limit, offset: offset)
+        
+        networkManager.fetch(endpoint: endpoint, type: PokemonList.self)
+            .subscribe{ [weak self] result in
+                switch result {
+                case .success(let pokemonListData):
+                    self?.pokemonList.onNext(pokemonListData.results)
+                case .failure(let error):
+                    self?.error.onNext(error.localizedDescription)
+                }
+            }
+            .disposed(by: disposeBag)
+    }
 }
